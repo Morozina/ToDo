14241503e04b75aba2956c9c5995ee720ee5bed4
Vladlk <zaryadina2014@gmail.com>
2022-12-16 14:15:52 +0100

    no message

---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Кот on 03.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
